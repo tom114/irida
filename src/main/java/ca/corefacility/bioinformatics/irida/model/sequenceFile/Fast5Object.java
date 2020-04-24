952591381b61c75d110be12f8b9177eefed2d889
Tom Matthews <thomas.matthews@canada.ca>
2020-04-24 15:24:25 -0500

    added shell of  initial fast5 object

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/sequenceFile/Fast5Object.java
@@ -0,0 +1,49 @@
+package ca.corefacility.bioinformatics.irida.model.sequenceFile;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.envers.Audited;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.collect.ImmutableSet;
+
+@Entity
+@Table(name = "fast5_object")
+@EntityListeners(AuditingEntityListener.class)
+@Audited
+public class Fast5Object extends SequencingObject {
+
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@NotNull
+	private SequenceFile file;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@JsonIgnore
+	public Set<SequenceFile> getFiles() {
+		return ImmutableSet.of(file);
+	}
+
+	/**
+	 * Throws {@link UnsupportedOperationException} because you should not be
+	 * able to update a file.
+	 */
+	@JsonIgnore
+	@Override
+	public void setModifiedDate(Date modifiedDate) {
+		throw new UnsupportedOperationException("Cannot update a sequence file");
+	}
+
+	@Override
+	public String getLabel() {
+		return file.getFileName()
+				.toString();
+	}
+}
